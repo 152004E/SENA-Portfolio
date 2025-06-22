98efb9cb07e62d3b1a0e539f0bd0a2eb4b860b5a
emerson reyes <reyesemerson643@gmail.com>
2025-06-22 17:11:42 -0500

    agregando proyecto intent

---
+++ Java-proyectos/AppIntent/app/src/main/java/sena/myprojects/ficha2922188/appintent/MainActivity2.java
@@ -0,0 +1,42 @@
+package sena.myprojects.ficha2922188.appintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+    Button btnAccion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btnAccion = findViewById(R.id.button2);
+        btnAccion.setOnClickListener( v ->{
+            Intent intent = new Intent(MainActivity2.this,MainActivity.class);
+
+            startActivity(intent);
+
+
+        });;
+        String dato = getIntent().getStringExtra("variable");
+        Toast.makeText(this,dato,Toast.LENGTH_LONG).show();
+
+
+
+
+    }
+}
