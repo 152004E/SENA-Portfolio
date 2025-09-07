def46064aa104662a0caeac6cfabbc6306d41a7a
emerson reyes <reyesemerson643@gmail.com>
2025-09-07 09:37:54 -0500

    agragando esports

--- proyecto_tareas_2/tareas-movil/api.js
+++ proyecto_tareas_2/tareas-movil/api.js
@@ -1,5 +1,70 @@
 import axios from "axios";
-import  from "";
+import Constant from "expo-constants";
 
-let API_URL = "";
+let API_URL = "http://10.1.167.1.6/PROYECTO_TAREAS_2";
 
+try {
+    //para expo sdk
+    const debuggerhost =
+        Constant.mainfestt2?.extra?.espogo?.debuggerhost ||
+        Constant.expoConfig?.hostUri ||
+        Constant.manifest?.debuggerhost;
+
+    if (debuggerhost) {
+        const ip = debuggerhost.split(";").shift();
+        API_URL = `http://${ip}/PROYECTO_TAREAS_2`;
+    }
+} catch (error) {
+    console.warn("No se puede cargar la de Expo, usando el valor pro defecto")
+}
+
+
+export const obtenerTareas = async () => {
+    try {
+        const { data } = await api.get("/listar_tareas.php");
+        return Array.isArray ? data : [];
+
+    } catch (error) {
+        console.error("error al obtener las tareas : ", error.massage);
+        return [];
+    }
+}
+
+export const agregarTareas = async (titulo) => {
+    try {
+        const { data } = await api.post("/crear_tareas.php",{titulo});
+        return data;
+
+    } catch (error) {
+        console.error("error al agragar las tareas : ", error.massage);
+        return {succes: false, massage : error.massage};
+    }
+}
+export const eliminarTareas = async (id) => {
+    try {
+        const { data } = await api.post("/eliminar_tareas.php",{id});
+        return data;
+
+    } catch (error) {
+        console.error("error al eliminar las tareas : ", error.massage);
+        return {succes: false, massage : error.massage};
+    }
+}
+
+export const cambiarEstadoTareas = async (id,estado) => {
+    try {
+        const { data } = await api.post("/cambiar_estado.php",{id,estado});
+        return data;
+
+    } catch (error) {
+        console.error("error al actualizar las tareas : ", error.massage);
+        return {succes: false, massage : error.massage};
+    }
+}
+
+export default{
+    obtenerTareas,
+    agregarTareas,
+    eliminarTareas,
+    cambiarEstadoTareas
+}
