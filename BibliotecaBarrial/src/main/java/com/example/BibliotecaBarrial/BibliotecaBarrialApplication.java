59eb8313256daa37b793be963ac9fec08afd091f
Emerson Reyes <reyesemerson643@gmail.com>
2025-09-14 14:41:53 -0500

    new file, bibliotecaBarrial

---
+++ BibliotecaBarrial/src/main/java/com/example/BibliotecaBarrial/BibliotecaBarrialApplication.java
@@ -0,0 +1,13 @@
+package com.example.BibliotecaBarrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaBarrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaBarrialApplication.class, args);
+	}
+
+}
