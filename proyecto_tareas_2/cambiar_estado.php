939d8992d753f4dfdb0cc148243d6748cdc8ff58
Emerson Reyes <reyesemerson643@gmail.com>
2025-09-14 13:11:14 -0500

    final de dia, queda pendiente el axios y las importaciones

--- proyecto_tareas_2/cambiar_estado.php
+++ proyecto_tareas_2/cambiar_estado.php
@@ -6,21 +6,33 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Content-Type: application/json; charset=UTF-8");
 
 $datos = json_decode(file_get_contents("php://input"), true);
-
+if(!$datos || !isset($datos['id']) || !isset($datos['estado'])) {
+    echo json_encode([
+        'success' => false,
+        'message' => 'Datos inválidos.'
+    ]);
+    exit;
+}
 $id = intval($datos['id']);
 $estado= $datos['estado'] === 'pendiente' ? 'completado' : 'pendiente';
 
 $sql = "UPDATE tareas1 SET estado = '$estado' WHERE id = $id";
-if ($conexion->query($sql)) {
-    echo json_encode(["ssuccess" => true, "message" => "Estado de tarea cambiado exitosamente"]);
+if($conexion->query($sql)){
+    echo json_encode([
+        'success' => true,
+        'message' => 'Estado cambiado correctamente.'
+    ]);
 } else {
-    echo json_encode(["success" => false, "message" => "Error al cambiar el estado de la tarea: " . $conexion->error]);
+    echo json_encode([
+        'success' => false,
+        'message' => 'Error al cambiar el estado: ' . $conexion->error
+    ]);
 }
  
 ?>
 
 
-
+<!-- 
 
 // include ("conexion.php");
 
@@ -56,4 +68,4 @@ if ($conexion->query($sql)) {
 //     ]);
 // }
 
-// 
+//  -->
