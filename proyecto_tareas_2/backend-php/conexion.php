ef156abcb193eefba6c2ef1390306dcfd2dff677
Emerson Reyes <reyesemerson643@gmail.com>
2025-09-24 16:09:54 -0500

    Conexion a la base de datos

--- proyecto_tareas_2/backend-php/conexion.php
+++ proyecto_tareas_2/backend-php/conexion.php
@@ -1,14 +1,24 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
 
 $servidor = "localhost";
 $usuario = "root";
 $password = "";
 $base_Datos = "tareas_db";
 $puerto = 33065;
-
-$conexion = new mysqli($servidor, $usuario, $password, $base_Datos, $puerto);
-
-if ($conexion->connect_error) {
-    die("La conexion ha fallado: " . $conexion->connect_error);
+try {
+    $conexion = new mysqli($servidor, $usuario, $contrasena, $base_datos);
+    if ($conexion->connect_error) {
+        throw new Exception("Error de conexión: " . $conexion->connect_error);
+        $conexion->set_charset("utf8");
+    }
+} catch (Exception $e) {
+    header('Content-Type: application/json; charset=UTF-8');
+    echo json_encode([
+        "success" => false,
+        "message" => "No se pudo conectar a la base de datos",
+        "error" => $e->getMessage()
+    ]);
+    exit;
 }
