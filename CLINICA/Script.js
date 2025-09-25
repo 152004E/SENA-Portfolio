049a4a58eddf13a0f0918c150d8c3ec4f516eaf3
Emerson Reyes <reyesemerson643@gmail.com>
2025-09-25 17:24:52 -0500

    Method of php

--- CLINICA/Script.js
+++ CLINICA/Script.js
@@ -1,2 +1,20 @@
 console.log("Js conectado")
 
+const modal = document.getElementById("modalCrearUsuario");
+  const contenido = document.getElementById("contenidoModal");
+
+  function abrirModal() {
+    modal.classList.remove("hidden");
+    setTimeout(() => {
+      contenido.classList.remove("scale-95", "opacity-0");
+      contenido.classList.add("scale-100", "opacity-100");
+    }, 10);
+  }
+
+  function cerrarModal() {
+    contenido.classList.remove("scale-100", "opacity-100");
+    contenido.classList.add("scale-95", "opacity-0");
+    setTimeout(() => {
+      modal.classList.add("hidden");
+    }, 500); // mismo tiempo que la transición
+  }
--- CLINICA/clinicaPhp/pacientes.php
+++ CLINICA/clinicaPhp/pacientes.php
@@ -1,2 +1,66 @@
 <?php
 include "conexion.php";
+header("Content-Type: application/json");
+
+
+$method = $_SERVER('REQUEST_METHOD');
+
+if ($method === "POST"){
+    $data = json_encode(file_get_contents("php://input"),true);
+
+    $nombre = $data["nombre"];
+    $documento = $data["documento"];
+    $telefono = $data["telefono"];
+    $correo = $data["correo"];
+
+    $sql = "INSERT INTO pacientes (nombre,documento,telefono,correo) VALUES ($nombre,$documento,$telefono,$correo)";
+
+
+    if( $conexion->query($sql) === true){
+        echo json_encode([
+            "success" => true, "message" => "Paciente agregado",
+        ]);
+    }else{
+        echo json_encode([
+        "success" => false, "error" => $conexion->error
+        ]);
+    }
+    exit;
+}
+
+
+if ($method === "GET") {
+    $result = $conexion->query("SELECT * FROM pacientes");
+    $pacientes = [];
+    while($row = $result->fetch_assoc()){
+        $pacientes = $row;
+    }
+
+    echo json_encode($pacientes);
+    exit;
+}
+
+
+if ($method === "PUT"){
+    $data = json_encode(file_get_contents("php://input"),true);
+
+    $id = $data["id"];
+    $nombre = $data["nombre"];
+    $documento = $data["documento"];
+    $telefono = $data["telefono"];
+    $correo = $data["correo"];
+
+    $sql = "UPDATE pacientes SET nombre='$nombre',documento='$documento',telefono=$telefono', correo='$correo' WHERE $id";
+
+
+    if( $conexion->query($sql) === true){
+        echo json_encode([
+            "success" => true, "message" => "Paciente Actualizado",
+        ]);
+    }else{
+        echo json_encode([
+        "success" => false, "error" => $conexion->error
+        ]);
+    }
+    exit;
+}
