1936dcb73fcd1262e95cf36e0075d1e705449f33
Emerson Reyes <reyesemerson643@gmail.com>
2025-09-29 16:43:30 -0500

    Readme modification

--- CLINICA/dentalsoft/src/api/config.js
+++ CLINICA/dentalsoft/src/api/config.js
@@ -5,9 +5,10 @@
 //   citas: `${API_BASE_URL}/citas.php`
 // };
 
-const API_BASE_URL = 'http://10.194.133.148/SENA-Portfolio/CLINICA/clinicaPhp';
+const API_BASE_URL = 'http://192.168.2.4/SENA-Portfolio/CLINICA/clinicaPhp';
 
 export const API_ENDPOINTS = {
   pacientes: `${API_BASE_URL}/pacientes.php`,
   citas: `${API_BASE_URL}/citas.php`
 };
+
