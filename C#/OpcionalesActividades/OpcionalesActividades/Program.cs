106e18c7783581bc74c11cfa82e2b855363f1935
Emerson Reyes <reyesemerson643@gmail.com>
2025-11-13 22:10:11 -0500

    Proyectos c#

---
+++ C#/OpcionalesActividades/OpcionalesActividades/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Persona
+{
+    // Atributos
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+
+    // Constructor
+    public Persona(string nombre, int edad)
+    {
+        Nombre = nombre;
+        Edad = edad;
+    }
+
+    // Método
+    public void Saludar()
+    {
+        Console.WriteLine($"¡Hola! Mi nombre es {Nombre} y tengo {Edad} años.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Persona persona1 = new Persona("emerson reyes", 20);
+        persona1.Saludar();
+    }
+}
